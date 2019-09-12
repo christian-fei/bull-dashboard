75315689b2b3308976c897255cdc6fb28e0eb3bf
Christian Fei <crifei93@gmail.com>
2019-09-12 19:51:05 +0200

    horiz layout

--- client/components/Queue.js
+++ client/components/Queue.js
@@ -10,15 +10,15 @@ export default class Queue extends Component {
     return preact.h('div', { className: `queue queue-${queue.name}` }, [
       preact.h('h1', { className: 'queue-name' }, [queue.name]),
       // preact.h(Chart, { data: chartFor(queue[state.showQueueType]) }),
+      preact.h('div', { className: 'queue-types' }, [
+        ['active', 'completed', 'failed', 'waiting', 'delayed'].map(type => preact.h('div', {
+          onClick: () => updateState({ showQueue: queue.name, showQueueType: type })
+        }, [
+          preact.h('span', { className: 'queue-type-name' }, [type]),
+          preact.h('span', { className: 'queue-type-count' }, [queue[`${type}Length`]])
+        ]))
+      ]),
       preact.h('div', { className: 'queue-details' }, [
-        preact.h('div', { className: 'queue-types' }, [
-          ['active', 'completed', 'failed', 'waiting', 'delayed'].map(type => preact.h('div', {
-            onClick: () => updateState({ showQueue: queue.name, showQueueType: type })
-          }, [
-            preact.h('span', { className: 'queue-type-name' }, [type]),
-            preact.h('span', { className: 'queue-type-count' }, [queue[`${type}Length`]])
-          ]))
-        ]),
         preact.h('div', { className: 'queue-preview' }, queue[`${state.showQueueType}Length`] && queue[`${state.showQueueType}Length`] > 0 ? [
           preact.h('ul', { className: '' }, queue[state.showQueueType].map(job => preact.h(Job, job))),
           preact.h('pre', { className: '' }, [
