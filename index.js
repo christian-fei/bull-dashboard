c3c355b4f66f28f851a32ddcc69348f1598bd100
Christian Fei <crifei93@gmail.com>
2019-11-07 23:43:50 +0100

    cleanup

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
 
 async function queuesFromRedis (client, namespace = 'bull') {
   const queuesWithJobs = await client.keys(`${namespace}:*`)
-  // const namespaceRegExp = new RegExp(namespace + ':([^:]+)')
   const namespaceRegExp = /:([^:]+)/
   const queueNames = Array.from(new Set(queuesWithJobs
     .map(q => q.match(namespaceRegExp))
@@ -16,31 +15,6 @@ async function queuesFromRedis (client, namespace = 'bull') {
     .filter(x => x.indexOf(':') === -1)
     .filter(x => x !== '[object Object]')
   ))
-  // console.log({ queuesWithJobs })
+
   return queueNames.map(queueName => new Queue(queueName))
 }
-
-// async function set (key, value) {
-//   if (typeof value === 'object') value = JSON.stringify(value)
-//   key = key.toLowerCase()
-//   return _set(key, value)
-// }
-
-// function setFor (key) {
-//   return (value) => set(key, value)
-// }
-
-// async function get (pattern) {
-//   const keysForPattern = await _keys(pattern)
-//   const acc = []
-//   for (const key of keysForPattern) {
-//     let item = await _get(key)
-//     if (item.startsWith('{')) item = JSON.parse(item)
-//     acc.push(item)
-//   }
-//   return acc
-// }
-
-// async function del (pattern) {
-//   _del(pattern)
-// }
